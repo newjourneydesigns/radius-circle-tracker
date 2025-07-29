0251dcf3bc641da023396dd4f2d2cec34a756507
tripochenskivcc <trip.ochenski@valleycreek.org>
2025-07-28 20:52:40 -0500

    Fix edit button onclick to use window.router.currentPage instead of app.currentPage

--- pages/profile.js
+++ pages/profile.js
@@ -292,13 +292,13 @@ export default class ProfilePage {
                             <p class="text-sm text-gray-500 dark:text-gray-400">${window.utils.formatDate(date)}</p>
                         </div>
                         <div class="flex space-x-2">
-                            <button onclick="app.currentPage.editEntry('${entry.id}', '${entry.type}')" 
+                            <button onclick="window.router.currentPage.editEntry('${entry.id}', '${entry.type}')" 
                                     class="text-gray-400 hover:text-gray-600 dark:hover:text-gray-300">
                                 <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                                     <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
                                 </svg>
                             </button>
-                            <button onclick="app.currentPage.deleteEntry('${entry.id}', '${entry.type}')" 
+                            <button onclick="window.router.currentPage.deleteEntry('${entry.id}', '${entry.type}')" 
                                     class="text-red-400 hover:text-red-600">
                                 <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
